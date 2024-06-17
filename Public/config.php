9d6dbf15d6207023f455aeabd6ef7bd0f70d2d06
SW410 <123305489+SW410-Coder@users.noreply.github.com>
2024-06-17 11:02:06 +0530

    Add files via upload

---
+++ Public/config.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'username' => 'strike',
+        'password' => '123',
+        'database' => 'strikepractice',
+    ],
+    'server' => [
+        'name' => 'ServerName',
+        'url' => 'https://ServerName.net',
+        'store_url' => 'https://store.ServerName.net',
+        'staff_url' => 'https://staff.ServerName.net',
+        'support_url' => 'https://discord.ServerName.net',
+    ],
+    'arenas' => [
+        'lms' => 'LMS',
+        'brackets' => 'Brackets',
+        'party_vs_party_wins' => 'Party vs Party Wins',
+        'elo_nodebuffelo' => 'No Debuff ELO',
+        'elo_debuffelo' => 'Debuff ELO',
+        'elo_builduhcelo' => 'Build UHC ELO',
+        'elo_gappleelo' => 'Gapple ELO',
+        'elo_soupelo' => 'Soup ELO',
+        'elo_comboelo' => 'Combo ELO',
+        'elo_sumoelo' => 'Sumo ELO',
+        'elo_sumobestof3elo' => 'Sumo Best of 3 ELO',
+        'elo_nodebuffelopremium' => 'No Debuff ELO Premium',
+        'elo_hcfelo' => 'HCF ELO',
+        'elo_axepvpelo' => 'Axe PvP ELO',
+    ],
+];
+?>
